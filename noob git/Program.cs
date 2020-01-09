2e287bbe942241025f51fc4d99029da7cc495051
Max Mustonen Sjölund <max.mustonensjolund@learnet.se>
2020-01-09 12:49:04 +0100

    Add project files.

---
+++ noob git/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace noob_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.writeline("Penis sugen");
+            console.readkey;
+
+        }
+    }
+}
